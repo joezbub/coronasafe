6f4e760b77b96c523f236054b845971061452ec7
r2dev2bb8 <ronak.badhe@gmail.com>
2020-04-09 19:28:34 -0700

    Some more stuff

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import Geolocation from '@react-native-community/geolocation';
 import logo from './logo.svg';
 import './App.css';
 
+
+// Doesn't work yet
+function sendCoords(coords, url) {
+	console.log(url);
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET", url);
+	xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+	xmlhttp.send(coords.latitude.toString() + ' ' + coords.longitude.toString());
+	console.log("sent");
+}
+
 function App() {
   Geolocation.getCurrentPosition(info => console.log(info));
+  Geolocation.getCurrentPosition(info => sendCoords(info.coords, "localhost:8090"));
   return (
     <div className="App">
       <header className="App-header">
