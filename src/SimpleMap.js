01604116732e3d6fc5951b0f5138ef50553bf3fd
TevinWang <43354492+TevinWang@users.noreply.github.com>
2020-04-14 15:54:34 -0700

    Update SimpleMap.js

--- src/SimpleMap.js
+++ src/SimpleMap.js
@@ -1,112 +1,60 @@
-// import React from 'react';
-// import Geolocation from '@react-native-community/geolocation';
-// import logo from './logo.svg';
-// import './App.css';
-// import GoogleMapReact from 'google-map-react';
- 
-// const AnyReactComponent = ({ text }) => <div>{text}</div>;
- 
 
-// Doesn't work yet
-
-
-// function App() {
-//   Geolocation.getCurrentPosition(info => document.getElementById("latitude").innerHTML = info.coords.latitude);
-//   Geolocation.getCurrentPosition(info => document.getElementById("longitude").innerHTML = info.coords.longitude);
-//   Geolocation.getCurrentPosition(info => sendCoords(info.coords, "localhost:8090"));
-//   return (
-//     <div sclassName="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>Latitude:</p>
-//         <p id="latitude"></p>
-//         <p>Longitude:</p>
-//         <p id="longitude"></p>
-//         <p id="position">this is cool</p>
-//         {/* <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p> */}
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App; 
 
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
-import $ from "jquery";
-import Geolocation from '@react-native-community/geolocation';
 import { geolocated } from "react-geolocated";
 import MyGreatPlace from './MyGreatPlace.js';
 
 
-// const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
+//Sends current coordinates to url
 function sendCoords(coords, url) {
   var xmlhttp = new XMLHttpRequest();
-  // $.ajax({
-  //   type : "GET", 
-  //   url : "https://rawpythontest.r2dev2bb8.repl.co/", 
-  //   beforeSend: function(xhr){xhr.setRequestHeader("Coords", coords.latitude.toString() + ' ' + coords.longitude.toString())},
-  //   success : function(result) { 
-  //       console.log(result.responseText);
-  //   }, 
-  //   error : function(result) { 
-  //     console.log(result.responseText);
-  //   } 
-  // }); 
-  
   xmlhttp.open("POST", url);  
   xmlhttp.setRequestHeader("coords", coords.latitude.toString() + ' ' + coords.longitude.toString());
   xmlhttp.send(coords.latitude.toString() + ' ' + coords.longitude.toString());
   console.log(xmlhttp.responseText);
 	console.log("sent: " + coords.latitude.toString() + ' ' + coords.longitude.toString());
 }
-
+//Formats coordinates for rendering
 function parseCoords(coords) {
-  latlon = coords.slice(1, -1).split(', ');
-  lat = parseFloat(latlon[0]);
-  lon = parseFloat(latlon[1]);
+  var latlon = coords.slice(1, -1).split(', ');
+  var lat = parseFloat(latlon[0]);
+  var lon = parseFloat(latlon[1]);
   return {
     "latitude": lat,
     "longitude": lon
   };
 }
-
+//Sends a request to the server in order to get coordinates of every point
 function getCoords(coords, url) {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.open("GET", url);  
   xmlhttp.setRequestHeader("Coords", coords.latitude.toString() + ' ' + coords.longitude.toString());
   xmlhttp.send(coords.latitude.toString() + ' ' + coords.longitude.toString());
-  newcoords = xmlhttp.split('\n')
+  var newcoords = xmlhttp.response.split('\n');
   for (var i=0; i<newcoords.length; i++) {
     newcoords[i] = parseCoords(newcoords[i]);
   }
-  return xmlhttp.responseText;
+  return newcoords;
 }
+// Not used -> form posting 
 // xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
 
 class SimpleMap extends Component {
-  // double latitude;
- 
-  // SimpleMap() {
-  //   latitude = 0.0;
-  // }
   constructor(props) {
     super(props);
     this.state = {latitude: 0.0, longitude: 0.0};
-  
+  }
+  // create points on maps
+  createPoints(url, coords) {
+      var coordsList =  getCoords(coords, url);
+      var table = []
+      for (var i=0; i< coordsList.length; i++) {
+        table.push(<MyGreatPlace lat={coordsList[i].latitude} lng={coordsList[i].longitude} />);
+      }
+      return table;
 
-    
   }
   static defaultProps = {
     center: {
@@ -116,24 +64,23 @@ class SimpleMap extends Component {
     zoom: 11
   };
   render() {
+    // testing code
     // Geolocation.getCurrentPosition(
     //   position => {
     //     this.setState({latitude: position.coords.latitude, longitude: position.coords.longitude});
     //     sendCoords(this.state, "https://RawPythonTest.r2dev2bb8.repl.co");
     //   })
     // this.setState({latitude: this.props.latitude, longitude: this.props.longitude});
-    if (this.props.coords)  {
-      sendCoords(this.props.coords, "https://RawPythonTest.r2dev2bb8.repl.co");
-      console.log(getCoords(this.props.coords, "https://RawPythonTest.r2dev2bb8.repl.co"));
-    }
-  
+    // if (this.props.coords)  {
+    //   sendCoords(this.props.coords, "https://RawPythonTest.r2dev2bb8.repl.co");
+    //   console.log(getCoords(this.props.coords, "https://RawPythonTest.r2dev2bb8.repl.co"));
+    // } 
 
     return !this.props.isGeolocationAvailable ? (
       <div>Your browser does not support Geolocation</div>
   ) : !this.props.isGeolocationEnabled ? (
       <div>Geolocation is not enabled</div>
   ) : this.props.coords ? (
-      
       // Important! Always set the container height explicitly
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
@@ -141,7 +88,8 @@ class SimpleMap extends Component {
           defaultCenter={{lat: this.props.coords.latitude, lng: this.props.coords.longitude}}
           defaultZoom={16}
         >
-          <MyGreatPlace lat={this.props.coords.latitude} lng={this.props.coords.longitude} /* Kreyser Avrora */ />
+          {this.createPoints('https://RawPythonTest.r2dev2bb8.repl.co', this.props.coords)}
+          {/* <MyGreatPlace lat={this.props.coords.latitude} lng={this.props.coords.longitude} /> */}
           {/* <AnyReactComponent
             lat={this.state.latitude}
             lng={this.state.longitude}
